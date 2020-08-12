bbe923fc5e85b815d606e988191ea18b5b05965b
MaJiGerman <43379084+MaJiGerman@users.noreply.github.com>
2020-08-12 18:25:56 +0200

    refrescar una página con datos introducidos en ella

--- BlueAntWeb/src/main/java/es/urjc/etsii/blueantweb/Controllers/StatsController.java
+++ BlueAntWeb/src/main/java/es/urjc/etsii/blueantweb/Controllers/StatsController.java
@@ -3,13 +3,15 @@ package es.urjc.etsii.blueantweb.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StatsController {
 
 	@RequestMapping(value = { "/stats" })
-	public String home(Model model) {
-		model.addAttribute("name", "GERMAN");
+	public String stats_name(@RequestParam(defaultValue="german", value="usrName") String name, Model model) {
+		model.addAttribute("name", name);
 		return "stats_template";
 	}
+
 }
