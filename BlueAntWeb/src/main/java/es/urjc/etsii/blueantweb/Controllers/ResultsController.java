d45560913d9c249b8292091494ee2c71fe858641
MaJiGerman <43379084+MaJiGerman@users.noreply.github.com>
2021-04-21 20:27:56 +0200

    añadidos calses personalizadas
    creadas nuevas clases para organizar los datos consultados

--- BlueAntWeb/src/main/java/es/urjc/etsii/blueantweb/Controllers/ResultsController.java
+++ BlueAntWeb/src/main/java/es/urjc/etsii/blueantweb/Controllers/ResultsController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import es.urjc.etsii.blueantweb.Entities.Estadistica;
 import es.urjc.etsii.blueantweb.Entities.Partida;
 import es.urjc.etsii.blueantweb.Entities.Usuario;
 import es.urjc.etsii.blueantweb.Repositories.EstadisticaRepository;
@@ -29,21 +30,48 @@ public class ResultsController {
 	private PartidaRepository matchRepo;
 	
 	@RequestMapping(value = { "/results" })
-	public String results(@RequestParam String tabla, @RequestParam String columna, @RequestParam(required = false, defaultValue = "-1") String genero, @RequestParam(required = false) String rangoDesde, @RequestParam(required = false) String rangoHasta, Model model) {
-		System.out.println("tabla: " + tabla);
+	public String results(@RequestParam String division, @RequestParam String columna, @RequestParam(required = false, defaultValue = "-1") String genero, 
+			@RequestParam(required = false) String rangoDesde, @RequestParam(required = false) String rangoHasta, 
+			@RequestParam(required = false) String solo_centros, Model model) {
+		
+		System.out.println("division: " + division);
 		System.out.println("columna: " + columna);
 		System.out.println("rangoDesde: " + rangoDesde);
 		System.out.println("rangoHasta: " + rangoHasta);
 		System.out.println("genero: " + genero);
-		/*
-		if (String.valueOf(tabla) == "usuario"){
-	 	*/
+		System.out.println("Solo Centros: " + solo_centros);
+		
 		List<Usuario> usuarios_final = new ArrayList<>();
 		List<Usuario> usuarios = new ArrayList<>();
 		List<Usuario> usuarios_1 = new ArrayList<>();
 		List<Usuario> usuarios_0 = new ArrayList<>();
 
-		System.out.println("AAAAAA");
+		class myPartidaObject{
+			public int _idPartida;
+			public List<Estadistica> lista_estadisticas;
+			 protected myPartidaObject(int id) {
+				 this._idPartida = id;
+				 this.lista_estadisticas = new ArrayList<>();
+			 }
+	    }
+		
+		class myUsuarioObject{
+			public int _idUsuario;
+			public List<myPartidaObject> _lista_partidas;
+			 protected myUsuarioObject(int id) {
+				 this._idUsuario = id;
+				 this._lista_partidas = new ArrayList<>();
+			 }
+			 protected void setmyPartidaObject(List<myPartidaObject> lista_p) {
+				 this._lista_partidas = lista_p;
+			 }
+			 protected void addmyPartidaObject(myPartidaObject p) {
+				 this._lista_partidas.add(p);
+			 }
+	    }
+
+		System.out.println("--------------------------");
+		
 		if(columna.equals("edad"))
 		{	
 			System.out.println("SE BUSCA POR EDAD");
@@ -51,6 +79,7 @@ public class ResultsController {
 				usuarios = userRepo.findByAgeAndGender(Integer.parseInt(rangoDesde), Integer.parseInt(rangoHasta),Integer.parseInt(genero));
 			else
 				usuarios = userRepo.findByAge(Integer.parseInt(rangoDesde), Integer.parseInt(rangoHasta));
+			
 			if (!usuarios.isEmpty() && usuarios.size() > 0) {
 				System.out.println("se han encontrado "+usuarios.size()+" resultados");
 			    Usuario u = usuarios.get(0);
@@ -91,28 +120,11 @@ public class ResultsController {
 		for(int i=0; i<usuarios_final.size(); i++) {
 			Usuario aux_u = usuarios_final.get(i);
 			aux_lista_consulta.add(aux_u.getId());
-			/*
-			System.out.println("Usuario: " + aux_u.getId());
-			List<Integer> lista_partidas = matchRepo.findByIdUsuario2(aux_u.getId());
-			System.out.println("Tiene " + lista_partidas.size() + " partidas.");
-			// si ha participado en partidas, buscar sus datos
-			if(lista_partidas!=null || !lista_partidas.isEmpty())
-			{
-				for(int j=0; j<lista_partidas.size(); j++) {
-					int aux_p_id = lista_partidas.get(j);
-					System.out.println("  Partida con id: " + aux_p_id);
-					List<String> lista_estadistica = statsRepo.findByIdPartida(aux_p_id);
-					System.out.println("  Tiene " + lista_estadistica.size() + " subpartidas.");
-					
-					for(int k=0; k<lista_estadistica.size(); k++) {
-						String aux_len_sub_par = lista_estadistica.get(k);
-						System.out.println("    La subpartida ha durado " + aux_len_sub_par);
-					}
-				}
-			}
-			*/
 		}
 
+		/*
+		 * BUSCAR TODAS LAS PARTIDAS EN LAS QUE HAN PARTICIPADO LOS USUARIOS FILTRADOS COMO USUARIO2
+		 */
 		List<Object[]> lista_partidas = new ArrayList<>();
 		int sublista_ini = 0;
 		int sublista_fin = 2000;
@@ -133,22 +145,38 @@ public class ResultsController {
 			}
 		}
 		
-		HashMap<Integer, List<Integer>> hashDatosUsr = new HashMap<Integer, List<Integer>>();
+		/*
+		 * CREAR UN HASH MAP (TIEMPO ACCESO O(1)-O(N)) CON ID USR Y MI PROPIA CLASE
+		 */
+		
+		HashMap<Integer, myUsuarioObject> hashDatosUsr = new HashMap<Integer, myUsuarioObject>();
 		
 		for (Object[] par : lista_partidas) {
-		      System.out.println("Id: " + par[0] + ", IdUsuario2: " + par[1]);
+		      System.out.println("Id Partida: " + par[0] + ", Id Usuario2: " + par[1]);
 		      if(hashDatosUsr.containsKey(par[1])) {
-		    	  List<Integer> id_partidas = hashDatosUsr.get((Integer) par[1]);
-		    	  id_partidas.add((Integer) par[0]);
-		    	  hashDatosUsr.put((Integer) par[1], id_partidas);
+		    	  /*
+		    	   * Si esta el id del usr en el mapa, se obtiene el objeto myUsuarioObject asociado a ese id.
+		    	   * Se le añade a la lista de myPartidaObject la partida actual 
+		    	   */
+		    	  myUsuarioObject u = hashDatosUsr.get(par[1]);
+		    	  myPartidaObject p = new myPartidaObject((Integer) par[0]);
+		    	  u.addmyPartidaObject(p);
+		    	  hashDatosUsr.put((Integer) par[1], u);
 		      }else {
-		    	  List<Integer> id_partidas = new ArrayList<>();
-		    	  id_partidas.add((Integer) par[0]);
-		    	  hashDatosUsr.put((Integer) par[1], id_partidas);
+		    	  /*
+		    	   * Si no esta el id del usr en el mapa, se crea un objeto myUsuarioObject nuevo.
+		    	   * Se le añade una partida a la lista de myPartidaObject con las estadisticas vacias
+		    	   */
+		    	  myUsuarioObject u = new myUsuarioObject((Integer) par[1]);
+		    	  myPartidaObject p = new myPartidaObject((Integer) par[0]);
+		    	  List<myPartidaObject> aux_lista_p = new ArrayList<>();
+		    	  aux_lista_p.add(p);
+		    	  u.setmyPartidaObject(aux_lista_p);
+		    	  hashDatosUsr.put((Integer) par[1], u);
 		      }
 		  }
-			
-		System.out.println("BBBBBBBBB");
+
+		System.out.println("--------------------------");
 		
 		model.addAttribute("name", "DEFAULT");
 		model.addAttribute("resultados", usuarios_final);
